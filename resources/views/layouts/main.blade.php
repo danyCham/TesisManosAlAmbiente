db65408eb8e13b3be2b6253fe33f59df98cec17c
IsaiasRonquillo97 <44557629+IsaiasRonquillo97@users.noreply.github.com>
2020-08-31 00:14:42 -0500

    MOdificacion de main para q sea dinamico el menu

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -71,43 +71,7 @@
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <li class="nav-item has-treeview menu-open">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-lock"></i>
-              <p>
-                Seguridad
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-            
-              <li class="nav-item">
-                <a href="{{route('usuario.index')}}" class="{{ request()->routeIs('usuario.index') ? 'nav-link active' : 'nav-link' }}">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Usuarios</p>
-                </a>
-              </li>
-
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Asignacion Menú</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item has-treeview menu-open">
-            <a href="#" class="nav-link active">
-              <i class="fas fa-palette"></i>
-              <p>
-                Galería
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
+          
           @foreach(session()->get('Menu') as $item)
               <li class="nav-item has-treeview menu-open">
                 <a href="#" class="nav-link active">
